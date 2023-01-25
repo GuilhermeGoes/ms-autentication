d615682268ca468f48691f5042cc0c0cae27f322
GuilhermeGoes <guilhermegoes76@hotmail.com>
2023-01-25 20:45:48 -0300

    initial commit

---
+++ ms-autentication/src/main/java/com/eldorado/microservice/autentication/MsAutenticationApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.microservice.autentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAutenticationApplication.class, args);
+	}
+
+}
---
+++ ms-autentication/src/test/java/com/eldorado/microservice/autentication/MsAutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldorado.microservice.autentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAutenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
